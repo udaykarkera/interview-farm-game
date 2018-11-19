a05f2d368f879a2cecd12838ea127eee5082c026
Uday <uday@mobicom.com>
2018-11-19 11:47:09 +0530

    Add webpage with litle css

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Farm Game</title>
+    <link rel="stylesheet" type="text/css" href="css/home-page.css">
+</head>
+<body>
+    <table id="fed-chart">
+    </table>
+    <p id="display-win"></p>
+    <p id="display-msg"></p>
+    <p id="display-result"></p>
+    <br/>
+    <button id="feed-anyone" disabled="">Click or Press Enter to Feed One</button>
+    <button id="restart-game">Restart</button>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="js/farm-game.js"></script>
+</body>
+</html>
