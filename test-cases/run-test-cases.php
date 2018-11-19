454c6fae1a6add9d825e0797e1102e4caf322d37
Uday <uday@mobicom.com>
2018-11-19 16:35:47 +0530

    Give full path to a require in php

--- test-cases/run-test-cases.php
+++ test-cases/run-test-cases.php
@@ -5,7 +5,7 @@ spl_autoload_register('myAutoloader');
 function myAutoloader($class_name)
 {
     if (strpos($class_name, 'Test') !== false)
-        require_once $class_name.'.php';
+        require_once __DIR__. '/' .$class_name.'.php';
     else
         require_once __DIR__.'/../Classes/' . $class_name.'.php';
 }
